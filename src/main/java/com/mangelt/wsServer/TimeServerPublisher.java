0a7c306897afb363bac01e7b26561210f211df3e
Miguel Angel Torres Renteria (Vendor) <vn0x53q@mx.wal-mart.com>
2016-11-14 16:31:57 -0600

    Commit start, How create a web services with SOAP

---
+++ src/main/java/com/mangelt/wsServer/TimeServerPublisher.java
@@ -0,0 +1,14 @@
+package com.mangelt.wsServer;
+
+import javax.xml.ws.Endpoint;
+
+import com.mangelt.wsServer.sib.TimeServerImpl;
+
+public class TimeServerPublisher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Endpoint.publish("http://127.0.0.1:9090/ts", new TimeServerImpl());
+	}
+
+}
---
+++ src/main/java/com/mangelt/wsServer/sib/TimeServerImpl.java
@@ -0,0 +1,31 @@
+package com.mangelt.wsServer.sib;
+
+import java.util.Date;
+
+import javax.jws.WebService;
+
+import com.mangelt.wsServer.sei.TimeServer;
+
+/**
+* The @WebService property endpointInterface links the
+* SIB (this class) to the SEI (ch01.ts.TimeServer).
+* Note that the method implementations are not annotated
+* as @WebMethods.
+*/
+
+@WebService(endpointInterface = "com.mangelt.wsServer.sei.TimeServer")
+public class TimeServerImpl implements TimeServer {
+
+	@Override
+	public String getTimeAsString() {
+		// TODO return a date since string 
+		return new  Date().toString();
+	}
+
+	@Override
+	public long getTimeAsElapsed() {
+		// TODO return a date to time
+		return new Date().getTime();
+	}
+
+}
